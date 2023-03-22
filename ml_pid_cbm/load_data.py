5490f08d080b358588b4734afc88446dac5e0cc1
julnow <jul.nowk@gmail.com>
2023-03-22 13:16:57 +0100

    reformatted and added unit tests

--- ml_pid_cbm/load_data.py
+++ ml_pid_cbm/load_data.py
@@ -1,45 +1,108 @@
-
-from hipe4ml.tree_handler import TreeHandler
 import json
+from hipe4ml.tree_handler import TreeHandler
+
 
 class LoadData:
+    """
+    Class for loading data stored in .tree format into hipe4ml.TreeHandler
+    for training and testing ml model
+    """
 
-    def __init__(self, data_file_name: str, json_file_name: str, lower_p_cut: float, upper_p_cut: float, anti_particles: bool):
-        self.data_file_name = data_file_name 
-        self.lower_p_cut    = lower_p_cut
-        self.upper_p_cut    = upper_p_cut
+    def __init__(
+        self,
+        data_file_name: str,
+        json_file_name: str,
+        lower_p_cut: float,
+        upper_p_cut: float,
+        anti_particles: bool,
+    ):
+        self.data_file_name = data_file_name
+        self.lower_p_cut = lower_p_cut
+        self.upper_p_cut = upper_p_cut
         self.anti_particles = anti_particles
         self.json_file_name = json_file_name
 
-    #load file with data into hipe4ml TreeHandler
-    def load_file(self, data_file_name: str = None, tree_type: str = 'plain_tree') -> TreeHandler:
+    # load file with data into hipe4ml TreeHandler
+    def load_tree(
+        self, data_file_name: str = None, tree_type: str = "plain_tree"
+    ) -> TreeHandler:
+        """Loads tree from given file into hipe4ml TreeHandler
+
+        Args:
+            data_file_name (str, optional): name of the file with the tree. Defaults to None.
+            tree_type (str, optional): type of the tree structure to be loaded.
+            Defaults to "plain_tree".
+
+        Returns:
+            TreeHandler: hipe4ml structure contatining tree to train and test model on
+        """
+
         if data_file_name is None:
             data_file_name = self.data_file_name
-        return self.load_tree(data_file_name, tree_type)
-    
-    #load tree using quality cuts defined in json file
-    def load_tree(self, data_file_name: str, tree_type: str, json_file_name: str = None):
+        tree_handler = TreeHandler(data_file_name, tree_type)
+
+        return self.clean_tree(tree_handler, self.json_file_name)
+
+    def clean_tree(
+        self, tree_handler: TreeHandler, json_file_name: str = None
+    ) -> TreeHandler:
+        """Takes already loaded tree from TreeHandler and makes quality cuts
+            defined in the json file, and momentum cut defined in class definition
+
+        Args:
+            tree_handler (TreeHandler): TreeHandler contating tree to be cleaned
+            json_file_name (str, optional): Name of the json file containg
+            quality cuts definition. Defaults to None.
+
+        Returns:
+            TreeHandler: _description_
+        """
         if json_file_name is None:
             json_file_name = self.json_file_name
-        th = TreeHandler(data_file_name, tree_type)
         quality_cuts = self.load_quality_cuts(json_file_name)
         for cut in quality_cuts:
-            th = th.get_subset(cut)
-        #include specific momentum cut
-        p_cut = self.create_cut_string(self.lower_p_cut, "Complex_p", self.upper_p_cut)
-        th = th.get_subset(p_cut)
-        return th
-    
-    #loads qualit cuts from json file
-    def load_quality_cuts(self, json_filename: str):
-        with open(json_filename, "r") as f:
-            cuts = json.load(f)["cuts"]
-        quality_cuts = [self.__class__.create_cut_string(cut_data['lower'], cut_data['upper'], cut_name) for cut_name, cut_data in cuts.items()]
+            tree_handler = tree_handler.get_subset(cut)
+        # include specific momentum cut
+        p_cut = self.create_cut_string(self.lower_p_cut, self.upper_p_cut, "Complex_p")
+        tree_handler = tree_handler.get_subset(p_cut)
+        # include sign of charge
+        if self.anti_particles is False:
+            tree_handler = tree_handler.get_subset("Complex_q > 0")
+        elif self.anti_particles is True:
+            tree_handler = tree_handler.get_subset("Complex_q < 0")
+
+        return tree_handler
+
+    def load_quality_cuts(self, json_filename: str):  # -> list[str]:
+        """Loads quality cuts defined in json file into array of strings
+
+        Args:
+            json_filename (str): Name of the json file containg defined cuts
+
+        Returns:
+            list[str]: List of strings containg cuts definitions
+        """
+        with open(json_filename, "r") as json_file:
+            cuts = json.load(json_file)["cuts"]
+        quality_cuts = [
+            self.__class__.create_cut_string(
+                cut_data["lower"], cut_data["upper"], cut_name
+            )
+            for cut_name, cut_data in cuts.items()
+        ]
         return quality_cuts
 
-    #create string for cuts when loading data with precision of one digit after comma
     @staticmethod
     def create_cut_string(lower: float, upper: float, cut_name: str) -> str:
+        """Creates cut string for hipe4ml loader in format "lower_value < cut_name < upper_value"
+
+        Args:
+            lower (float): Value of lower cut, 1 decimal place
+            upper (float): Value of upper cut, 1 decimal place
+            cut_name (str): Name of the cut variable
+
+        Returns:
+            str: Formatted string in format "lower_value < cut_name < upper_value"
+        """
         cut_string = f"{lower:.1f} < {cut_name} < {upper:.1f}"
         return cut_string
-    
--- ml_pid_cbm/test_load_data.py
+++ ml_pid_cbm/test_load_data.py
@@ -1,29 +1,86 @@
-
 import unittest
-from load_data import LoadData
 from unittest.mock import patch, mock_open
+from hipe4ml.tree_handler import TreeHandler
+import pandas as pd
+from load_data import LoadData
+
 
 class TestLoadData(unittest.TestCase):
-
     def setUp(self):
-        self.loader_1 = LoadData(data_file_name = "data.tree", json_file_name = "config.json", lower_p_cut = 0., upper_p_cut = 1., anti_particles = False)
-
+        self.loader_pos = LoadData(
+            data_file_name="data.tree",
+            json_file_name="config.json",
+            lower_p_cut=0.0,
+            upper_p_cut=3.0,
+            anti_particles=False,
+        )
+        self.loader_anti = LoadData(
+            data_file_name="data.tree",
+            json_file_name="config.json",
+            lower_p_cut=0.0,
+            upper_p_cut=3.0,
+            anti_particles=True,
+        )
+        self.loader_empty = LoadData(
+            data_file_name=None,
+            json_file_name=None,
+            lower_p_cut=0.0,
+            upper_p_cut=3.0,
+            anti_particles=True,
+        )
 
     def test_create_cut_string(self):
-        #ideally formatted
+        # ideally formatted
         expected_string = "0.1 < test_cut < 13.0"
-        self.assertEqual(LoadData.create_cut_string(.1, 13., "test_cut"), expected_string)
-        #not ideally formatted
-        self.assertEqual(LoadData.create_cut_string(.141, 13, "test_cut"), expected_string)
+        self.assertEqual(
+            LoadData.create_cut_string(0.1, 13.0, "test_cut"), expected_string
+        )
+        # not ideally formatted
+        self.assertEqual(
+            LoadData.create_cut_string(0.141, 13, "test_cut"), expected_string
+        )
 
-            
     def test_load_quality_cuts(self):
         json_data = """{"cuts":{"Complex_mass2": {"lower": -1.0,"upper": 2.0},"Complex_pT": {"lower": 0.0,"upper": 2.0}}}"""
-        #mocking json file for testing
+        # mocking json file for testing
         with patch("builtins.open", mock_open(read_data=json_data)):
-            quality_cuts = self.loader_1.load_quality_cuts("test.json")
+            quality_cuts = self.loader_pos.load_quality_cuts("test.json")
             expected_cuts = ["-1.0 < Complex_mass2 < 2.0", "0.0 < Complex_pT < 2.0"]
             self.assertEqual(quality_cuts, expected_cuts)
-            
-    if __name__ == '__main__':
+
+    def test_clean_tree(self):
+        # manually created entries to test data cleaning
+        positive_entry = {"Complex_q": 1, "Complex_p": 2, "Complex_eta": 3}
+        incorrect_eta_entry = {"Complex_q": 1, "Complex_p": 3, "Complex_eta": 9}
+        negative_entry = {"Complex_q": -1, "Complex_p": 1, "Complex_eta": 2}
+        incorrect_p_entry = {"Complex_q": -1, "Complex_p": 30, "Complex_eta": 2}
+        incorrect_type_entry = {"Complex_q": 1, "Complex_p": 30, "Complex_eta": None}
+        complete_data = [
+            positive_entry,
+            incorrect_eta_entry,
+            negative_entry,
+            incorrect_p_entry,
+            incorrect_type_entry,
+        ]
+        # mock json file for testing
+        json_data = """{"cuts":{"Complex_p": {"lower": 0,"upper": 3.0},"Complex_eta": {"lower": 0.0,"upper": 6.0}}}"""
+        tree_handler = TreeHandler()
+        tree_handler.set_data_frame(pd.DataFrame(complete_data))
+        with patch("builtins.open", mock_open(read_data=json_data)):
+            # positive particles
+            positive_tree_handler = self.loader_pos.clean_tree(tree_handler)
+            pd.testing.assert_frame_equal(
+                positive_tree_handler.get_data_frame().reset_index(drop=True),
+                pd.DataFrame([positive_entry]).reset_index(drop=True),
+                check_dtype=False,
+            )
+            # negative (anti) particles
+            negative_tree_handler = self.loader_anti.clean_tree(tree_handler)
+            pd.testing.assert_frame_equal(
+                negative_tree_handler.get_data_frame().reset_index(drop=True),
+                pd.DataFrame([negative_entry]).reset_index(drop=True),
+                check_dtype=False,
+            )
+
+    if __name__ == "__main__":
         unittest.main()
