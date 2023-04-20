2418ac65e8459baa0880e7925b233b07ecc0c3b1
julnow <jul.nowk@gmail.com>
2023-04-20 10:31:56 +0200

    slurm config

--- ml_pid_cbm/slurm_ml_pid.sh
+++ ml_pid_cbm/slurm_ml_pid.sh
@@ -9,7 +9,7 @@ mlpidpath=$WORK/ml-pid-cbm/ml_pid_cbm
 export PATH=/lustre/cbm/users/jnowak/miniconda3/bin/:$PATH
 conda activate cbm23
 #get into folder for training
-CONFIG=$mlpidpath/test_config.json
+CONFIG=$mlpidpath/slurm_config.json
 cd $WORK/pid/
 #run training
 python $mlpidpath/train_model.py -c $CONFIG -p $(((INDEX-1)*3)) $((INDEX*3)) --saveplots | tee training_output_${INDEX}.txt
