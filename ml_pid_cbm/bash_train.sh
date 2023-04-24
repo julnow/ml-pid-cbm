082540f44b034ce8b157bb386eeda1d6fd442f7b
julnow <jul.nowk@gmail.com>
2023-04-25 00:52:01 +0200

    current bash scripts

--- ml_pid_cbm/bash_train.sh
+++ ml_pid_cbm/bash_train.sh
@@ -8,7 +8,6 @@ CONFIG="config.json"
 #positive and negative particles
 for i in 0 3 6 9 
 do
-    python -u train_model.py -c $CONFIG -p $i $((i+3)) --saveplots --hyperparams | tee 
-train_$i.txt
+    python -u ../train_model.py -c $CONFIG -p $i $((i+3)) --saveplots --hyperparams --nworkers 6 | tee train_from_$i.txt
     # python train_model.py -c $CONFIG -p $i $((i+3)) --saveplots --antiparticles 
 done
--- ml_pid_cbm/bash_validate.sh
+++ ml_pid_cbm/bash_validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conda init bash
+eval "$(conda shell.bash hook)"
 conda activate cbm22
 
 CONFIG="config.json"
@@ -15,9 +15,9 @@ for dir in model_*
         fi
 done
 #manual proba values
-python validate_model.py -c $CONFIG -m model_0.0_3.0_positive -p .92 .95 .9
-python validate_model.py -c $CONFIG -m model_3.0_6.0_positive -p .9 .9 .85
-python validate_model.py -c $CONFIG -m model_6.0_9.0_positive -p .6 .8 .55
-python validate_model.py -c $CONFIG -m model_9.0_12.0_positive -p .6 .6 .6
+python validate_model.py -c $CONFIG -m model_0.0_3.0_positive -p .7 .6 .7 --nworkers 6
+python validate_model.py -c $CONFIG -m model_3.0_6.0_positive -p .6 .6 .6 --nworkers 6
+python validate_model.py -c $CONFIG -m model_6.0_9.0_positive -p .6 .6 .6 --nworkers 6
+python validate_model.py -c $CONFIG -m model_9.0_12.0_positive -p .6 .4 .6 --nworkers 6
 #validation of all models
-python validate_multiple_models.py -c $CONFIG -m $readymodels
+python validate_multiple_models.py -c $CONFIG -m $readymodels --nworkers 4
