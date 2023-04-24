999d7cb25399395b64aa302b8ef8355d7c925203
julnow <jul.nowk@gmail.com>
2023-04-24 13:25:11 +0200

    tof data config

--- ml_pid_cbm/slurm_ml_pid.sh
+++ ml_pid_cbm/slurm_ml_pid.sh
@@ -4,11 +4,13 @@ INDEX=${SLURM_ARRAY_TASK_ID}
 WORK=/lustre/cbm/users/$USER
 #path to ml-pid-cbm python package
 mlpidpath=$WORK/ml-pid-cbm/ml_pid_cbm
-
-#load conda
-export PATH=/lustre/cbm/users/jnowak/miniconda3/bin/:$PATH
+#load conda from home directory on lustre
+export PATH=$work/miniconda3/bin/:$PATH
 eval "$(conda shell.bash hook)"
 conda activate cbm23
+#needed for pritning graphs as slurm doesn't find it automatically
+export FONTCONFIG_FILE=$CONDA_PREFIX/etc/fonts/fonts.conf
+export FONTCONFIG_PATH=$CONDA_PREFIX/etc/fonts/
 #get into folder for training
 CONFIG=$mlpidpath/slurm_config.json
 cd $WORK/pid/
