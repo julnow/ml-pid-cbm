c971a0b01b0674bfdb1ab1d3f53504c2a240582c
julnow <jul.nowk@gmail.com>
2023-04-28 15:48:51 +0200

    current version of bash scripts

--- ml_pid_cbm/bash_train.sh
+++ ml_pid_cbm/bash_train.sh
@@ -6,8 +6,11 @@ conda activate cbm22
 CONFIG="config.json"
 
 #positive and negative particles
-for i in 0 3 6 9 
+for i in 0 2 4 
 do
-    python -u ../train_model.py -c $CONFIG -p $i $((i+3)) --saveplots --hyperparams --nworkers 6 | tee train_from_$i.txt
-    # python train_model.py -c $CONFIG -p $i $((i+3)) --saveplots --antiparticles 
+    python -u ../../train_model.py -c $CONFIG -p $i $((i+2)) --saveplots --nworkers 10 | tee train_from_$i.txt
+done
+for i in 6 9 
+do
+    python -u ../../train_model.py -c $CONFIG -p $i $((i+3)) --saveplots --nworkers 10 | tee train_from_$i.txt 
 done
--- ml_pid_cbm/bash_validate.sh
+++ ml_pid_cbm/bash_validate.sh
@@ -11,13 +11,14 @@ for dir in model_*
         if [[ -d "$dir" ]]; then
             readymodels+="$dir "
             # echo $dir
-            #python validate_model.py -c $CONFIG -m $dir -p .9 .96 .94
-        fi
+            # python ../../validate_model.py -c $CONFIG -m $dir -n 4 -e .4 .95 40 
+       fi
 done
 #manual proba values
-python validate_model.py -c $CONFIG -m model_0.0_3.0_positive -p .7 .6 .7 --nworkers 6
-python validate_model.py -c $CONFIG -m model_3.0_6.0_positive -p .6 .6 .6 --nworkers 6
-python validate_model.py -c $CONFIG -m model_6.0_9.0_positive -p .6 .6 .6 --nworkers 6
-python validate_model.py -c $CONFIG -m model_9.0_12.0_positive -p .6 .4 .6 --nworkers 6
+#python ../../validate_model.py -c $CONFIG -m model_0.0_2.0_positive -n 4 -p .4 .95 .95
+#python ../../validate_model.py -c $CONFIG -m model_2.0_4.0_positive -n 4 -p .4 .4 .4
+#python ../../validate_model.py -c $CONFIG -m model_4.0_6.0_positive -n 4 -p .4 .4 .4
+#python ../../validate_model.py -c $CONFIG -m model_6.0_9.0_positive -n 4  -p .4 .95 .4
+#python ../../validate_model.py -c $CONFIG -m model_9.0_12.0_positive -n 4 -p .4 .4 .4
 #validation of all models
-python validate_multiple_models.py -c $CONFIG -m $readymodels --nworkers 4
+python ../../validate_multiple_models.py -c $CONFIG -m $readymodels --nworkers 4
