123b998f6ac09d9edf399a38c06b3b4778302b54
julnow <jul.nowk@gmail.com>
2023-05-11 09:17:08 +0200

    minor refactor

--- ml_pid_cbm/merge_pdf.sh
+++ ml_pid_cbm/merge_pdf.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 #validation of single models
+mkdir -p pdfs
 for dir in model_*
     do
         if [[ -d "$dir" ]]; then
-            pdfunite $dir/*.pdf $dir.pdf 
+            mkdir -p $dir/merge
+            cd $dir
+            pdfjam confusion_*.pdf --nup 2x1 --landscape --outfile merge/cm.pdf
+            pdfjam correlat*.pdf --nup 2x2 --landscape --outfile merge/corr.pdf
+            pdfjam eff_pT*.pdf plot_pt*.pdf --landscape --nup 4x2  --outfile merge/pT.pdf
+            pdfjam shap_*.pdf --nup 2x2 --landscape --outfile merge/shap.pdf
+            cp roc_plot.pdf merge/roc_plot.pdf
+            pdfjam tof_plot_*.pdf --nup 4x3 --landscape --outfile merge/tof.pdf
+            cp vars_dist*.pdf merge/
+            cd ../  
+            pdfunite $dir/merge/*.pdf pdfs/$dir.pdf 
        fi
 done
-pdfunite all_models/*.pdf all_models.pdf
+pdfunite all_models/*.pdf pdfs/all_models.pdf
