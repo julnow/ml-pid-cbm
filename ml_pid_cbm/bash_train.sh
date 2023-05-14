5047855a8ef06bff18d4e7baeef63f6ce630f503
julnow <jul.nowk@gmail.com>
2023-05-15 00:19:20 +0200

    2M events for training

--- ml_pid_cbm/bash_train.sh
+++ ml_pid_cbm/bash_train.sh
@@ -6,11 +6,8 @@ conda activate cbm22
 CONFIG="config.json"
 
 #positive and negative particles
-for i in 0 2 4 
+for i in 0 2 4 6 
 do
-    python -u ../../train_model.py -c $CONFIG -p $i $((i+2)) --saveplots --nworkers 10 | tee train_from_$i.txt
+    python -u ../../train_model.py -c $CONFIG -p $i $((i+2)) --saveplots --nworkers 16 --usevalidation --hyperparams | tee train_from_$i.txt
 done
-for i in 6 9 
-do
-    python -u ../../train_model.py -c $CONFIG -p $i $((i+3)) --saveplots --nworkers 10 | tee train_from_$i.txt 
-done
+python -u ../../train_model.py -c $CONFIG -p 8 12 --saveplots --nworkers 16 --usevalidation --hyperparams | tee train_from_8.txt
