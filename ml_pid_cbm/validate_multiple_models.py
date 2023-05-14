05a754372d2dc2c1a8a0c16ec92ddf676c3b604f
julnow <jul.nowk@gmail.com>
2023-05-15 00:43:32 +0200

    validate multiple models as seperate class

--- ml_pid_cbm/validate_multiple_models.py
+++ ml_pid_cbm/validate_multiple_models.py
@@ -2,31 +2,48 @@ import argparse
 import os
 from concurrent.futures import ThreadPoolExecutor
 from shutil import copy2
-from typing import Set
+from typing import List, Set
+import sys
 
 import pandas as pd
-from sklearn.metrics import confusion_matrix
 from validate_model import ValidateModel
 
+class ValidateMultipleModels(ValidateModel):
+    """
+    Class for validating data from multiple models. 
+    Inherits from ValidateModel
+    """
 
-def load_pickles(files_list: Set[str], n_workers: int = 1) -> pd.DataFrame:
-    """Loads multiple pickle files produced by validate_model module.
+    def __init__(self, json_file_name: str, files_list: Set[str], n_workers: int = 1):
+        super().__init__(-12, 12, False, json_file_name, None)
+        self.particles_df = self.load_pickles(files_list, n_workers)
+
+    @staticmethod
+    def load_pickles(files_list: Set[str], n_workers: int = 1) -> pd.DataFrame:
+        """Loads multiple pickle files produced by validate_model module.
+
+        Args:
+            files_list (Set[str]): Files list containg picle files with datasets.
+            n_workers (int, optional): Number of workers for multithreading. Defaults to 1.
+
+        Returns:
+            pd.DataFrame: Dataframe with merged datasets.
+        """
+        with ThreadPoolExecutor(max_workers=n_workers) as executor:
+            results = list(executor.map(pd.read_pickle, files_list))
+            whole_df = pd.concat(results, ignore_index=True)
+        return whole_df
+    
+def parse_args(args: List[str]) -> argparse.Namespace:
+    """
+    Arguments parser for the main method.
 
     Args:
-        files_list (Set[str]): Files list containg picle files with datasets.
-        n_workers (int, optional): Number of workers for multithreading. Defaults to 1.
+        args (List[str]): Arguments from the command line, should be sys.argv[1:].
 
     Returns:
-        pd.DataFrame: Dataframe with merged datasets.
+        argparse.Namespace: argparse.Namespace containg args
     """
-    with ThreadPoolExecutor(max_workers=n_workers) as executor:
-        results = list(executor.map(pd.read_pickle, files_list))
-        whole_df = pd.concat(results, ignore_index=True)
-    return whole_df
-
-
-if __name__ == "__main__":
-    # parser for main class
     parser = argparse.ArgumentParser(
         prog="ML_PID_CBM ValidateMultipleModels",
         description="Program for loading multiple validated PID ML models",
@@ -54,20 +71,22 @@ if __name__ == "__main__":
         default=1,
         help="Max number of workers for ThreadPoolExecutor which reads Root tree with data.",
     )
-    args = parser.parse_args()
+    return parser.parse_args(args)
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
     # config  arguments to be loaded from args
     json_file_name = args.config[0]
     models = args.modelnames
     n_workers = args.nworkers
     pickle_files = {f"{model}/validated_data.pickle" for model in models}
-    all_particles_df = load_pickles(pickle_files, n_workers)
     # new folder for all files
     json_file_path = os.path.join(os.getcwd(), json_file_name)
     if not os.path.exists("all_models"):
         os.makedirs("all_models")
     os.chdir("all_models")
     copy2(json_file_path, os.getcwd())
-    validate = ValidateModel(-12, 12, False, "", all_particles_df)
+    validate = ValidateMultipleModels(json_file_name, pickle_files, n_workers)
     # graphs
     validate.confusion_matrix_and_stats()
     print("Generating plots...")
